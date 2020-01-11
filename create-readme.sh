cdbdaa338961c52717d2a4accd23f48c515bfeda
avcourt <andrew.vcourt@gmail.com>
2020-01-10 23:21:03 -0600

    Use calling filename for readme generation

--- create-readme.sh
+++ create-readme.sh
@@ -4,21 +4,22 @@
 ## - creates a readme with a description of each script
 
 README=README.md
+S_NAME="$(basename "$0")"
 
 if [[ -f "$README" ]]; then
-    echo "$README already exists. Updating with new scripts."
+    echo "${README} already exists. Updating with new scripts."
 else
-    echo "Creating README with script information."
+    echo "Creating a readme with script information."
 fi
 
-printf "# create-readme\n\n" > $README
-printf "Creates a README with info about each script in this directory\n" >> $README
-printf "\n## Scripts\n" >> $README
+printf "# Scripts\n\n" > $README
+printf "Various shell scripts. This readme created by [%s](%s)\n" "$S_NAME" "$S_NAME" >> $README
+printf "\n## Script Descriptions\n" >> $README
 
 for f in *.sh; do
     {
         printf "\n### [%s](%s)\n\n" "$f" "$f"
-        sed -n '/^##/p' "$f" | tr -d '#'
-        printf "***\n"
+        sed -n '/^##/p' "$f" | cut -c 4-
+        printf "\n***\n"
      } >> $README
 done
