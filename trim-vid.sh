105a0fc0285f772b17eac578219c81fe85010153
avcourt <andrew.vcourt@gmail.com>
2020-01-17 12:31:20 -0600

    Add usage message on incorrect command args

--- trim-vid.sh
+++ trim-vid.sh
@@ -4,6 +4,11 @@
 ## - Trims the end off of a video
 ## - prompts for removal of original and renaming of trimmed version
 
+if [ $# -ne 2 ]; then
+    echo "Usage: trim-vid <input.vid> <time in x:x:x>"
+    exit 1
+fi
+
 if [ -f "$1" ]; then
     if ffmpeg -noaccurate_seek -i "$1" -to "$2" -c copy trimmed-"$1" ; then
         echo 
@@ -32,5 +37,5 @@ if [ -f "$1" ]; then
         echo "Error: Trim failed"
     fi
 else 
-    echo "Error: $1 does not exist"
+    echo "Error: File $1 does not exist"
 fi
